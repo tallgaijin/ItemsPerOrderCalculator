04f70d362f2c2e0823a10e6f1891377ea5e5a670
Adam Thompson <thompson.adamj@gmail.com>
2018-12-05 13:54:32 -0500

    Initial

--- ItemsPerOrderCalculator/Program.cs
+++ ItemsPerOrderCalculator/Program.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +12,32 @@ namespace ItemsPerOrderCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.WriteLine("Enter the full path to your text file of transaction ids. Example: C:\\Users\\bob\\Desktop\\domains.txt");
+            Console.WriteLine("One id per line, like this:");
+            Console.WriteLine("fdgh45yjdt");
+            Console.WriteLine("zdgfhe4567");
+            Console.WriteLine("Please do not close the program until you see: Done!");
+            string userInput = Console.ReadLine();
+            string[] listOfIds = File.ReadAllLines(userInput);
+            Console.WriteLine(listOfIds.Length);
+            var csv = new StringBuilder();
+            var firstLine = string.Format("Trans ID,Items");
+            csv.AppendLine(firstLine);
+            var IdInstances = new ConcurrentDictionary<string, int>();
+
+            foreach (var line in listOfIds)
+            {
+                IdInstances.AddOrUpdate(line, 1, (key, value) => value + 1);
+            }
+
+            foreach (var trans in IdInstances)
+            {
+                var newLine = string.Format("{0},{1}", trans.Key, trans.Value);
+                csv.AppendLine(newLine);
+            }
+
+            File.WriteAllText("ssl-checker-output.csv", csv.ToString());
+
         }
     }
 }
